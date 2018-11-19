f929eb7a637b2fb2d5624003ab47ffb87d12f6f1
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-11-19 10:25:20 -0500

    Updated from Brython Server: 11/19/2018 10:25:20 AM

--- Calc1.py
+++ Calc1.py
@@ -1,1 +1,28 @@
+#calc RRAM, LRAM, MRAM
 
+a = float(input('Enter left bound'))
+b = float(input('Enter right bound'))
+n = int(input('Enter a number of subintervals'))
+f = input('Enter the function')
+
+LRAM = 0.0
+RRAM = 0.0
+MRAM = 0.0
+
+x = a
+dX = (b-a)/n
+
+for i in range(n):
+    x1 = x
+    f1 = eval(f)
+    LRAM = LRAM + f1*dX
+    x = x+dX
+    x2 = x
+    f2 = eval(f)
+    RRAM = RRAM + f2*dX
+    x = (x1 +x2)/2
+    fm = eval(f)
+    MRAM = MRAM + fm*dX
+    x = x2
+    
+print('LRAM =',LRAM,'RRAM =',RRAM,'MRAM =', MRAM)
