001aa6736f8e9d877357a3fe0a3d31c2b10e0180
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-12-04 10:35:20 -0500

    Updated from Brython Server: 12/4/2018 10:35:20 AM

--- Calc1.py
+++ Calc1.py
@@ -10,6 +10,7 @@ LRAM = 0.0
 RRAM = 0.0
 MRAM = 0.0
 TRAM = 0.0
+SRAM = 0.0
 
 x = a
 dX = (b-a)/n
@@ -26,6 +27,7 @@ for i in range(n):
     x = (x1 +x2)/2
     fm = eval(f)
     MRAM = MRAM + fm*dX
+    SRAM = SRAM + (dX/3)*(f1+(4*fm)+f2)
     x = x2
     
 print('LRAM =',LRAM,'RRAM =',RRAM,'MRAM =', MRAM, 'Trapezoidal Rule =', TRAM)
